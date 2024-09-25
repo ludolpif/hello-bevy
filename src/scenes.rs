8b9918d4e5eb5f6b1bfefee28a6c11c94c7a6f2c
ludolpif <ludolpif@gmail.com>
2024-09-25 16:00:03 +0200

    scenes: drop saving, will be completly changed with incoming bevy

--- src/scenes.rs
+++ src/scenes.rs
@@ -1,5 +1,4 @@
-use bevy::{prelude::*, tasks::IoTaskPool};
-use std::{fs::File, io::Write};
+use bevy::prelude::*;
 
 use crate::sources::ColorSourceComponent;
 
@@ -10,7 +9,6 @@ impl Plugin for ScenePersistancePlugin {
         app
             .add_systems(Startup, (
                     Self::hello_world,
-                    //Self::save_scene_system,
                     Self::load_scene_system,
             ))
             .add_systems(Update, Self::log_system)
@@ -21,10 +19,11 @@ impl Plugin for ScenePersistancePlugin {
 // The initial scene file will be loaded below and not change when the scene is saved
 const SCENE_FILE_PATH: &str = "scenes/load_scene_example.scn.ron";
 
-// The new, updated scene data will be saved here so that you can see the changes
-const NEW_SCENE_FILE_PATH: &str = "scenes/load_scene_example-new.scn.ron";
-
 impl ScenePersistancePlugin {
+    //XXX: don't make too much code here, Bevy will got a .bsn file format !
+    // https://github.com/bevyengine/bevy/discussions/9538
+    // For now code is borrowed from https://github.com/bevyengine/bevy/blob/main/examples/scene/scene.rs
+
     fn hello_world() {
         info!("registered ScenePersistancePlugin");
     }
@@ -48,61 +47,5 @@ impl ScenePersistancePlugin {
             // note: needs #[derive(Debug,...) on pub struct ColorSourceComponent
             info!("Entity({}) contains {:?}", entity.index(), csc);
         }
-    }
-
-    fn save_scene_system(world: &mut World) {
-        // Scenes can be created from any ECS World.
-        // You can either create a new one for the scene or use the current World.
-        // For demonstration purposes, we'll create a new one.
-        let mut scene_world = World::new();
-
-        // The `TypeRegistry` resource contains information about all registered types (including components).
-        // This is used to construct scenes, so we'll want to ensure that our previous type registrations
-        // exist in this new scene world as well.
-        // To do this, we can simply clone the `AppTypeRegistry` resource.
-        let type_registry = world.resource::<AppTypeRegistry>().clone();
-        scene_world.insert_resource(type_registry);
-
-        /*
-        let mut component_b = ComponentB::from_world(world);
-        component_b.value = "hello".to_string();
-        scene_world.spawn((
-                component_b,
-                ComponentA { x: 1.0, y: 2.0 },
-                Transform::IDENTITY,
-                Name::new("joe"),
-        ));
-        scene_world.spawn(ComponentA { x: 3.0, y: 4.0 });
-        scene_world.insert_resource(ResourceA { score: 1 });
-        */
-        scene_world.spawn(ColorSourceComponent {
-            native_size: Vec2::new(1920.0, 1080.0),
-            transform: Transform::IDENTITY,
-            color: Color::linear_rgba(1.0,0.0,0.0,0.5)
-        } );
-        // With our sample world ready to go, we can now create our scene using DynamicScene or DynamicSceneBuilder.
-        // For simplicity, we will create our scene using DynamicScene:
-        let scene = DynamicScene::from_world(&scene_world);
-
-        // Scenes can be serialized like this:
-        let type_registry = world.resource::<AppTypeRegistry>();
-        let type_registry = type_registry.read();
-        let serialized_scene = scene.serialize(&type_registry).unwrap();
-
-        // Showing the scene in the console
-        info!("{}", serialized_scene);
-
-        // Writing the scene to a new file. Using a task to avoid calling the filesystem APIs in a system
-        // as they are blocking
-        // This can't work in WASM as there is no filesystem access
-        #[cfg(not(target_arch = "wasm32"))]
-        IoTaskPool::get()
-            .spawn(async move {
-                // Write the scene RON data to file
-                File::create(format!("assets/{NEW_SCENE_FILE_PATH}"))
-                    .and_then(|mut file| file.write(serialized_scene.as_bytes()))
-                    .expect("Error while writing scene to file");
-                })
-        .detach();
     }
 }
