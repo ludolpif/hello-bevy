3206b57fc87a3f508ae491e8303b09c16123053d
ludolpif <ludolpif@gmail.com>
2024-09-28 17:06:31 +0200

    Replace "bevy_dev_console" by "bevy-inspector-egui"

--- src/devmode.rs
+++ src/devmode.rs
@@ -1,40 +1,57 @@
+use bevy::input::common_conditions::input_toggle_active;
 use bevy::prelude::*;
-use bevy::log::LogPlugin;
 use bevy::dev_tools::fps_overlay::{FpsOverlayConfig, FpsOverlayPlugin};
 use bevy::window::{PresentMode,WindowMode,WindowResolution};
-use bevy_dev_console::prelude::*;
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
 pub struct DevModePlugin;
 
 impl Plugin for DevModePlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins((
-                // Add the log plugin with the custom log layer
-                DefaultPlugins.set(LogPlugin {
-                    custom_layer: custom_log_layer,
-                    ..default()
-                }).set(WindowPlugin {
-                    primary_window: Some(Window {
-                        present_mode: PresentMode::AutoNoVsync,
-                        mode: WindowMode::BorderlessFullscreen,
-                        resolution: WindowResolution::new(1920.0, 1080.0)
-                            .with_scale_factor_override(1.0),
-                            ..default()
+        app
+            .add_plugins((
+                    // Add the defaults plugins with no framerate limiting
+                    DefaultPlugins.set(WindowPlugin {
+                        primary_window: Some(Window {
+                            present_mode: PresentMode::AutoNoVsync,
+                            mode: WindowMode::BorderlessFullscreen,
+                            resolution: WindowResolution::new(1920.0, 1080.0)
+                                .with_scale_factor_override(1.0),
+                                ..default()
+                        }),
+                        ..default()
                     }),
-                    ..default()
-                }),
-                // Add the dev console plugin itself.
-                DevConsolePlugin,
-                // Add the FpsOverlayPlugin with it's config
-                FpsOverlayPlugin { config:
-                    FpsOverlayConfig {
-                        text_config: TextStyle {
-                            font_size: 32.0,
-                            color: Color::srgb(0.0, 1.0, 0.0),
-                            font: default(),
+                    // Add the bevy world inspector default UI (could be closed with a key)
+                    WorldInspectorPlugin::default().run_if(input_toggle_active(true, KeyCode::Escape)),
+                    // Add the FpsOverlayPlugin with it's config
+                    FpsOverlayPlugin { config:
+                        FpsOverlayConfig {
+                            text_config: TextStyle {
+                                font_size: 16.0,
+                                color: Color::srgb(0.0, 1.0, 0.0),
+                                font: default(),
+                            }
                         }
-                    }
-                },
-        )) ;
+                    },
+            ))
+            .add_systems(Startup, (
+                    Self::hello_world,
+            ))
+            .add_systems(Update, (
+                    Self::add_name_to_fpsoverlay_for_worldinspector,
+            ));
+    }
+}
+
+impl DevModePlugin {
+    fn hello_world() {
+        info!("registered DevModePlugin");
+    }
+    fn add_name_to_fpsoverlay_for_worldinspector(query: Query<Entity, (Without<Name>, With<Children>, With<ZIndex>)>, mut commands: Commands) {
+        for entity_id in query.iter() {
+            let name = Name::new("FpsOverlay");
+            info!("inserting Name {name:?} on {entity_id:?}");
+            commands.entity(entity_id).insert(name);
+        }
     }
 }
