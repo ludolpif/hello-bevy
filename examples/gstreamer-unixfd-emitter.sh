2f5a6c8dbb1b37424eef34dbc608eae6e028e8b9
ludolpif <ludolpif@gmail.com>
2025-06-30 23:04:06 +0200

    Disable gstreamer for now as rust-analyser hangs with cargo metadata git update

--- examples/gstreamer-unixfd-emitter.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-GST_DEBUG=3 gst-launch-1.0 videotestsrc num-buffers=300 is-live=true pattern=ball ! video/x-raw, width=1920, height=1080, framerate=30/1 ! unixfdsink socket-path=/tmp/lbs0
--- examples/gstreamer-unixfd-receiver.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-GST_DEBUG=3 gst-launch-1.0 unixfdsrc socket-path=/tmp/lbs0 ! queue ! autovideosink
